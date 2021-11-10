d41acc27727a4118da70938ddf834b3fed8de143
Rita-Gujar <ritagujar14@gmail.com>
2021-11-10 22:18:29 +0530

    #Day16 JavaScript Project Countdown Timer added date

---
+++ day-16/countdown-timer/app.js
@@ -0,0 +1,49 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+// console.log(items);
+
+// Months and days are Zero index based
+// If we pass 4 for month it will take month as a may
+let futureDate = new Date(2021, 11, 24, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+// console.log(months[month]);
+
+month = months[month];
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am `;
