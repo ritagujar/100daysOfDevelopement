8cbd14e66f231bc1d03064984455aae15cf9b764
Rita-Gujar <ritagujar14@gmail.com>
2021-11-12 00:21:40 +0530

    #Day17 - JavaScript Project Quiz App - added More question and Restart Button

--- day-17/quiz-app/app.js
+++ day-17/quiz-app/app.js
@@ -8,6 +8,10 @@ const answerButtonsElements = document.getElementById("answer-button");
 let shuffledQuestion, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
   console.log("Started");
@@ -40,13 +44,42 @@ function showQuestion(question) {
 }
 
 function resetState() {
+  clearStatusClass(document.body);
   nextButton.classList.add("hide");
   while (answerButtonsElements.firstChild) {
     answerButtonsElements.removeChild(answerButtonsElements.firstChild);
   }
 }
 
-function selectAnswer() {}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElements.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+
+  if (shuffledQuestion.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerHTML = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 const questions = [
   {
@@ -56,6 +89,33 @@ const questions = [
       { text: "8", correct: false },
       { text: "12", correct: false },
       { text: "5", correct: false },
+    ],
+  },
+  {
+    question: "What is 2 * 2?",
+    answers: [
+      { text: "4", correct: true },
+      { text: "8", correct: false },
+      { text: "12", correct: false },
+      { text: "5", correct: false },
+    ],
+  },
+  {
+    question: "What is 10 / 2?",
+    answers: [
+      { text: "4", correct: false },
+      { text: "8", correct: false },
+      { text: "12", correct: false },
+      { text: "5", correct: true },
+    ],
+  },
+  {
+    question: "What is 10 % 5?",
+    answers: [
+      { text: "4", correct: false },
+      { text: "3", correct: false },
+      { text: "0", correct: true },
+      { text: "1", correct: false },
     ],
   },
 ];
