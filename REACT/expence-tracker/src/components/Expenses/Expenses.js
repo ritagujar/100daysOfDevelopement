2762f7d464e4e5046e1bba4dd77406851bf10891
Rita-Gujar <ritagujar14@gmail.com>
2021-11-18 17:36:00 +0530

    #Day24 - React - Expense Tracker React State and Events

--- REACT/expence-tracker/src/components/Expenses/Expenses.js
+++ REACT/expence-tracker/src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenceItem from "./ExpenceItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangedHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangedHandler}
+      />
       <ExpenceItem
         title={props.items[0].title}
         amount={props.items[0].amount}
