deb0a0da645d3434cff3ca0019aea5a0b565a2d6
Rita-Gujar <ritagujar14@gmail.com>
2021-11-18 23:27:21 +0530

    #Day24 - React - Conditional Rendering, Rendering list of data

--- REACT/expence-tracker/src/App.js
+++ REACT/expence-tracker/src/App.js
@@ -1,38 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummy_expenses = [
+  {
+    id: "e1",
+    title: "Car Insurence",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "House Insurence",
+    amount: 294.12,
+    date: new Date(2021, 6, 3),
+  },
+  {
+    id: "e3",
+    title: " New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e4",
+    title: " New TV",
+    amount: 799.49,
+    date: new Date(2021, 10, 20),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurence",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "House Insurence",
-      amount: 294.12,
-      date: new Date(2021, 6, 3),
-    },
-    {
-      id: "e3",
-      title: " New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-    {
-      id: "e4",
-      title: " New TV",
-      amount: 799.49,
-      date: new Date(2021, 10, 20),
-    },
-  ];
+  const [expenses, setExpenses] = useState(dummy_expenses);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
