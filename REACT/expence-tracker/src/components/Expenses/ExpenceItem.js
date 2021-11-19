c4ae1edbd59ca2362c4debb61c633378f10ba81e
Rita-Gujar <ritagujar14@gmail.com>
2021-11-19 21:09:32 +0530

    #Day25 - React - Conditional Return and added a Chart Feature in Expense Tracker

--- REACT/expence-tracker/src/components/Expenses/ExpenceItem.js
+++ REACT/expence-tracker/src/components/Expenses/ExpenceItem.js
@@ -5,13 +5,15 @@ import Card from "../UI/Card";
 
 function ExpenceItem(props) {
   return (
-    <Card className="expence-item">
-      <ExpenceDate date={props.date} />
-      <div className="expence-item__description">
-        <h2>{props.title}</h2>
-        <div className="expence-item__price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expence-item">
+        <ExpenceDate date={props.date} />
+        <div className="expence-item__description">
+          <h2>{props.title}</h2>
+          <div className="expence-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 }
 
--- REACT/expence-tracker/src/components/Expenses/Expenses.js
+++ REACT/expence-tracker/src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenceItem from "./ExpenceItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -15,19 +16,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses Found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenceItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -35,7 +23,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangedHandler}
         />
-        {expensesContent}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
