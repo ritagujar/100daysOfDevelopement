9b82bf1fb058db9cc9047f02b47ada066de34dfc
Rita-Gujar <ritagujar14@gmail.com>
2021-11-22 10:41:21 +0530

    #day28 - React - Practice-project1 - User Input State and Adding Validation & Reseting Logic

--- REACT/practice-project1/src/components/Users/AddUser.js
+++ REACT/practice-project1/src/components/Users/AddUser.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "./../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    // Here Entered age is treated as a string as we passes Empty String to useState('') and in javascript input field also takes a input as a string. so to be safe add + infront of a variable. Althoug if we didn't add + it will work.
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="username">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
