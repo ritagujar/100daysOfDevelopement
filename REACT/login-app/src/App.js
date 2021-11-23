2f75130a388e5466dc979c837779dddf207e4dbc
Rita-Gujar <ritagujar14@gmail.com>
2021-11-23 21:45:33 +0530

    #Day29 - React - Login App - useEffect() Hook, Storing the state in Local Storage

--- REACT/login-app/src/App.js
+++ REACT/login-app/src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
@@ -7,13 +6,25 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // '1' is for loged in and '0' for log out - Just a string we can use any name
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
