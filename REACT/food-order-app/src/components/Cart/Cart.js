c3ab6b7d7630df32b675d027440b48903f2323b2
Rita-Gujar <ritagujar14@gmail.com>
2021-11-29 20:35:53 +0530

    #Day35 - React - Food Order App - Adding a Modal via React Portal (Added a Style on Modal)

--- REACT/food-order-app/src/components/Cart/Cart.js
+++ REACT/food-order-app/src/components/Cart/Cart.js
@@ -13,7 +13,7 @@ const Cart = (props) => {
 
   return (
     <Modal>
-      cartitems
+      {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
