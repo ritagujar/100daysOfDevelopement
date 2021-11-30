a78141782bdd4d32f92cc48978f92b0d620f352c
Rita-Gujar <ritagujar14@gmail.com>
2021-11-30 22:23:20 +0530

    #Day36 - React - Food Order App - Using the useEffect() Hook and added a bump Efect on Cart Button

--- REACT/food-order-app/src/components/Layout/HeaderCartButton.js
+++ REACT/food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "./../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   // Reduce is a built in method it is use to transform an array into single value or number.
   // Reduce method takes two arguments- first Function {} and second is a starting value 0
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  } `;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+      console.log("CLEANUP FUNCTION");
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
