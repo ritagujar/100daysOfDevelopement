144a17df47440057d90018009b406a2b5a525d86
Rita-Gujar <ritagujar14@gmail.com>
2021-11-30 16:46:39 +0530

    #Day36 - React - Food Order App - Using a Context

--- REACT/food-order-app/src/components/Layout/HeaderCartButton.js
+++ REACT/food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "./../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "./../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  // Reduce is a built in method it is use to transform an array into single value or number.
+  // Reduce method takes two arguments- first Function {} and second is a starting value 0
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
--- REACT/food-order-app/src/store/cart-context.js
+++ REACT/food-order-app/src/store/cart-context.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const CartContext = React.CreateContext({
-  item: [],
+const CartContext = React.createContext({
+  items: [],
   totalAmount: 0,
   addItem: (item) => {},
   removeItem: (id) => {},
