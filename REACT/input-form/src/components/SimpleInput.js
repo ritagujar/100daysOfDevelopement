58bfb88579a70a193ac3197b3db165576c08e1ac
Rita-Gujar <ritagujar14@gmail.com>
2021-12-02 22:46:09 +0530

    #Day38 - React - Input Form - Form Submition and Getting user input

--- REACT/input-form/src/components/SimpleInput.js
+++ REACT/input-form/src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import React, { useState, useRef } from "react";
+
 const SimpleInput = () => {
+  const nameInputRef = useRef();
+  const [enterdName, setEnteredname] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredname(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(enterdName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value("");  =>  NOT IDEAL, DON'T MANIPULATE THE DOM
+    setEnteredname("");
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enterdName}
+        />
       </div>
       <div className="form-action">
         <button>Submit</button>
