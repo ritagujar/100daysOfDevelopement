0ac78694a893729d6e4a68c7bbd26d72d0eb2502
Rita-Gujar <ritagujar14@gmail.com>
2021-12-02 11:02:22 +0530

    #Day38 - React - Counter-app

---
+++ REACT/counter-app/src/components/ForwardCounter.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+
+const ForwardCounter = () => {
+  const [couter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <Card>{couter}</Card>;
+};
+
+export default ForwardCounter;
