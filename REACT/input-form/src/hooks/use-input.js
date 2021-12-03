5f5813b101d73d14b29284be5a305c93190bcc64
Rita-Gujar <ritagujar14@gmail.com>
2021-12-03 21:24:53 +0530

    #Day39 - React - Input Form - Custom Hook Using useReducer()

--- REACT/input-form/src/hooks/use-input.js
+++ REACT/input-form/src/hooks/use-input.js
@@ -1,27 +1,54 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
+  }
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [entredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid = validateValue(entredValue);
-  const hasError = !valueIsValid && isTouched;
+  //   const [entredValue, setEnteredValue] = useState("");
+  //   const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangedHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
+    // setEnteredValue(event.target.value);
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
+    // setIsTouched(true);
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
+    // setEnteredValue("");
+    // setIsTouched(false);
   };
 
   return {
-    value: entredValue,
+    // value: entredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangedHandler,
