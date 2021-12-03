230557084c82c485f923925be86d0a45b4a1a90f
Rita-Gujar <ritagujar14@gmail.com>
2021-12-03 22:32:13 +0530

    #Day39 - React - Food Order App - Handling Errors

--- REACT/food-order-app/src/components/Meals/AvailableMeals.js
+++ REACT/food-order-app/src/components/Meals/AvailableMeals.js
@@ -6,12 +6,18 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isloading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-http-1f978-default-rtdb.firebaseio.com/meals.json"
+        "https://react-http-1f978-default-rtdb.firebasei.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,7 +35,10 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isloading) {
@@ -38,6 +47,10 @@ const AvailableMeals = () => {
         <p>Loading...</p>
       </section>
     );
+  }
+
+  if (httpError) {
+    return <section className={classes.mealsError}>{httpError}</section>;
   }
 
   const mealsList = meals.map((meal) => (
