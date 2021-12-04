f89008f034ddde5234fcd07f26c71e21fe0bb534
Rita-Gujar <ritagujar14@gmail.com>
2021-12-04 22:22:04 +0530

    #Day40 - React-Redux - Redux App - Redux Toolkit & Adding State Slices

--- REACT/redux-app/src/components/Counter.js
+++ REACT/redux-app/src/components/Counter.js
@@ -2,34 +2,35 @@ import classes from "./Counter.module.css";
 // import { Component } from "react";
 // import { connect } from 'react-redux'
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
-  /* useDispatch() Hook gives us back the dispatch function which we can execute.
-  dispatch is a fuction, a function which we can call, which will dispatch an action against our Redux store. */
+  // useDispatch() Hook gives us back the dispatch function which we can execute.
+  // dispatch is a fuction, a function which we can call, which will dispatch an action against our Redux store.
 
   const dispatch = useDispatch();
 
-  /* useSelector is used to get data out of the store.
-  The function we pass to useSelector will be executed by react-redux
-  which then basically determines which piece of data  we wanna extract from out store. */
+  // useSelector is used to get data out of the store.
+  //The function we pass to useSelector will be executed by react-redux
+  //which then basically determines which piece of data  we wanna extract from out store.
 
   const counter = useSelector((state) => state.counter);
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
@@ -48,9 +49,9 @@ const Counter = () => {
 
 export default Counter;
 
-/* Hooks are not usable in class-based components.
-   We can use react-redux 'connect' function, it is a function which helps to connect class-based components to redux.
-   Actully we could use 'connect' in functional components also,  but in functional components using hooks is more convenient */
+// Hooks are not usable in class-based components.We can use react-redux 'connect' function,
+//  it is a function which helps to connect class-based components to redux.
+// Actully we could use 'connect' in functional components also,  but in functional components using hooks is more convenient
 
 // class Counter extends Component {
 //   incrementHandler() {
@@ -92,7 +93,7 @@ export default Counter;
 //   };
 // };
 
-/* 'connect' when executed will return a new function as a value,
- which we then execute again and then we pass our component to that returned function as a argument. */
+// 'connect' when executed will return a new function as a value,
+//which we then execute again and then we pass our component to that returned function as a argument.
 
 // export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- REACT/redux-app/src/store/index.js
+++ REACT/redux-app/src/store/index.js
@@ -1,43 +1,77 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterStore = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
+// We prepare the slice of globle state.
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    // Here we don't need action becouse
+    // This methods will automatically called depending on which action was triggered
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
 
-      // We need to set the showCounter property here becouse we are returning the overall state object
-      // and Redux won't merge your changes with the existing state.
-      // It instead takes what you return and replaces the existing state with it.
-      showCounter: state.showCounter,
-    };
-  }
+    // Here we use action becouse we need a data that attached to the action (Payload).
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
+// const counterStore = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
+//       // We need to set the showCounter property here becouse we are returning the overall state object
+//       // and Redux won't merge your changes with the existing state.
+//       // It instead takes what you return and replaces the existing state with it.
 
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
+//       // Set all the other states when we update a piece of state becouse we override the old state.
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  return state;
-};
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-const store = createStore(counterStore);
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// const store = createStore(counterSlice.reducer);
+
+// confidureStore like createStore creates a store, but it makes merging multiple reducers into one reducer easier thereafter.
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
 export default store;
