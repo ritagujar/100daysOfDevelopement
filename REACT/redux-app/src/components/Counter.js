b8ecb9453e76c63f25e11d7b47a6446b53bdda86
Rita-Gujar <ritagujar14@gmail.com>
2021-12-04 15:54:33 +0530

    #Day40 - React-Redux - Redux App- Using useSelector() & useDispatch() Hooks and Dispatching the Actions From inside Components

--- REACT/redux-app/src/components/Counter.js
+++ REACT/redux-app/src/components/Counter.js
@@ -1,12 +1,36 @@
 import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+  // useDispatch() Hook gives us back the dispatch function which we can execute.
+  // dispatch is a fuction, a function which we can call, which will dispatch an action against our Redux store.
+
+  const dispatch = useDispatch();
+
+  // useSelector is used to get data out of the store.
+  //The function we pass to useSelector will be executed by react-redux
+  //which then basically determines which piece of data  we wanna extract from out store.
+
+  const increamentHandler = () => {
+    dispatch({ type: "increament" });
+  };
+
+  const decreamentHandler = () => {
+    dispatch({ type: "decreament" });
+  };
+
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={increamentHandler}>Increament</button>
+        <button onClick={decreamentHandler}>Decreament</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
