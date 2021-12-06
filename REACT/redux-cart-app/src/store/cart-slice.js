5ee0d8d91871934dfc7bfa2a1ba66ed5b04fe514
Rita-Gujar <ritagujar14@gmail.com>
2021-12-06 20:22:11 +0530

    #Day42 - React-Redux - Redus Cart App - Sending PUT request to the backend (Firebase)

--- REACT/redux-cart-app/src/store/cart-slice.js
+++ REACT/redux-cart-app/src/store/cart-slice.js
@@ -7,11 +7,14 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
-
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -33,11 +36,11 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
 
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
