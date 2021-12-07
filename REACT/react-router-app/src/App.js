f64971b6583c13dcdd6df9677e4c737026714ca5
Rita-Gujar <ritagujar14@gmail.com>
2021-12-08 02:31:35 +0530

    #Day44 - React-Router - Defining & Using Routes

---
+++ REACT/react-router-app/src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/welcome" element={<Welcome />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
