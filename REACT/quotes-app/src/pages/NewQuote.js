46743e8facb9fa641f45877ab6ba9c514ff15cdd
Rita-Gujar <ritagujar14@gmail.com>
2021-12-09 12:30:43 +0530

    #Day45 - React-Route - Quotes App - Implemented Programmatic Navigation

--- REACT/quotes-app/src/pages/NewQuote.js
+++ REACT/quotes-app/src/pages/NewQuote.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  // useHistory Hook allows us to change the browser history.
+  // The history page we visited.
+  // useHistory gives us history object
+
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    // Push - It pushes a new page on the state of pages.
+    // Replace - It replaces the current page
+
+    // Difference betweem push and replace -
+    // With push we can go back with the back button to the page we're comming from, with replace we can't
+    // So replace is like a redirect where we change a current page, push adds a new page
+
+    history.push("/quotes");
+    // history.replace("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
