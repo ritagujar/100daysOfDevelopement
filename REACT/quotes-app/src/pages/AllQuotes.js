6de36e4c994bd3c6f361858a295ef4163c692940
Rita-Gujar <ritagujar14@gmail.com>
2021-12-09 11:59:37 +0530

    #Day45 - React-Route - Quotes App - Adding Dummy Data, Form and more content

--- REACT/quotes-app/src/pages/AllQuotes.js
+++ REACT/quotes-app/src/pages/AllQuotes.js
@@ -1,11 +1,13 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+];
 
 const AllQuotes = () => {
-  return (
-    <div>
-      <h1>All Qutoes</h1>
-    </div>
-  );
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
