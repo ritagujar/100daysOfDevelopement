cd36714051fb90db71e6f60369caf2596eccb7c2
Rita-Gujar <ritagujar14@gmail.com>
2021-12-09 11:13:42 +0530

    #Day45 - React-Route - Quotes App - Practice Basics of React Router

---
+++ REACT/quotes-app/src/pages/QuoteDetail.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams, Route } from "react-router-dom";
+import Comments from "./../components/comments/Comments";
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>Quote Detail</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </div>
+  );
+};
+
+export default QuoteDetail;
