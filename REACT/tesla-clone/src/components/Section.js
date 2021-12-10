f25b0cc86090e4088d50facc214f3f0ea37d616f
Rita-Gujar <ritagujar14@gmail.com>
2021-12-11 00:06:17 +0530

    #Day46 - React - Tesla Clone - Added Fade Animation on Item Text and Buttons

--- REACT/tesla-clone/src/components/Section.js
+++ REACT/tesla-clone/src/components/Section.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 const Wrap = styled.div`
   width: 100vw;
@@ -66,15 +67,19 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroundImage}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="./images/down-arrow.svg"></DownArrow>
       </Buttons>
     </Wrap>
