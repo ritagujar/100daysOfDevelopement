ddedd2e7e93c00f2bed6e125c8f3b69bccdb4962
Rita-Gujar <ritagujar14@gmail.com>
2021-12-10 22:18:35 +0530

    #Day46 - React - Tesla Clone - Added Section

--- REACT/tesla-clone/src/components/Home.js
+++ REACT/tesla-clone/src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+const Container = styled.div`
+  height: 100vh;
+`;
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section />
+      <Section />
+      <Section />
+    </Container>
+  );
 }
 
 export default Home;
