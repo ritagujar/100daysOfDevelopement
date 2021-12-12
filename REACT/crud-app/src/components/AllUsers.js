498700063aea02c839c2b0862d8fae0338aa0bfc
Rita-Gujar <ritagujar14@gmail.com>
2021-12-12 18:23:41 +0530

    #Day48 - React - Crud App - Created Api And Fetch all data

--- REACT/crud-app/src/components/AllUsers.js
+++ REACT/crud-app/src/components/AllUsers.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableHead,
+  TableCell,
+  Paper,
+  TableRow,
+  TableBody,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import { getUsers } from "./../Service/api";
+
+const useStyles = makeStyles({
+  table: {
+    width: "90%",
+    margin: "50px 0 0 50px",
+  },
+  thead: {
+    "& > *": {
+      fontSize: 20,
+      background: "#000000",
+      color: "#ffffff",
+    },
+  },
+  row: {
+    "& > *": {
+      fontSize: 18,
+    },
+  },
+});
 
 const AllUsers = () => {
-  return <p>Hii From All user</p>;
+  const [users, setUsers] = useState([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const getAllUsers = async () => {
+    const response = await getUsers();
+    console.log(response);
+    setUsers(response.data);
+  };
+
+  return (
+    <Table className={classes.table}>
+      <TableHead>
+        <TableRow className={classes.thead}>
+          <TableCell>Id</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Username</TableCell>
+          <TableCell>Email</TableCell>
+          <TableCell>Phone</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow className={classes.row} key={user.id}>
+            <TableCell>{user.id}</TableCell>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>{user.username}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.phone}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default AllUsers;
