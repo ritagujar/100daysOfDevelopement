ac483521eaccf1963a8dc109c3980785b647baf1
Rita-Gujar <ritagujar14@gmail.com>
2021-12-12 21:16:42 +0530

    #Day48 - React - Crud App - Created POST Request using Axios

--- REACT/crud-app/src/components/AddUser.js
+++ REACT/crud-app/src/components/AddUser.js
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  FormGroup,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import { addUser } from "../Service/api";
+import { useNavigate } from "react-router-dom";
+
+const initailValue = {
+  name: "",
+  username: "",
+  email: "",
+  phone: "",
+};
+
+const useStyles = makeStyles({
+  container: {
+    width: "50%",
+    margin: "5% 0 0 25%",
+    "& > *": {
+      marginTop: 20,
+    },
+  },
+});
 
 const AddUser = () => {
-  return <p>Hello From Add Users</p>;
+  const [user, setUser] = useState(initailValue);
+  const { name, username, email, phone } = user;
+  const navigate = useNavigate();
+  const classes = useStyles();
+
+  const onValueChange = (e) => {
+    console.log(e.target.value);
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const addUserDetails = async () => {
+    await addUser(user);
+    navigate(-1);
+  };
+
+  return (
+    <FormGroup className={classes.container}>
+      <Typography variant="h4">Add User</Typography>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Name</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="name"
+          value={name}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Username</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="username"
+          value={username}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="email"
+          value={email}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Phone</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="phone"
+          value={phone}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => addUserDetails()}
+        >
+          Add User
+        </Button>
+      </FormControl>
+    </FormGroup>
+  );
 };
 
 export default AddUser;
