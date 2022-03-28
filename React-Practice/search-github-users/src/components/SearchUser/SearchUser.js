ef63f597320fdb87c8b5717cc2bf20befc35724f
ritagujar <ritagujar14@gmail.com>
2022-03-29 00:50:29 +0530

    Feteched the user data from github api using axios

--- React-Practice/search-github-users/src/components/SearchUser/SearchUser.js
+++ React-Practice/search-github-users/src/components/SearchUser/SearchUser.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchSection } from "./style";
+import axios from "axios";
+
+const github = axios.create({
+  baseURL: "https://api.github.com/",
+});
 
 const SearchUser = () => {
+  const [user, setUser] = useState("");
+  const [userData, setUserData] = useState([]);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const result = await github.get(`users/${user}`);
+      setUserData(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setUser("");
+  };
+
   return (
-    <SearchSection>
-      <form action="">
-        <input type="text" placeholder="Search for user" />
-        <button type="search">Search</button>
-      </form>
-    </SearchSection>
+    <>
+      <SearchSection>
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="text"
+            placeholder="Search for user"
+            value={user}
+            onChange={(event) => setUser(event.target.value)}
+          />
+          <button type="search">Search</button>
+        </form>
+      </SearchSection>
+    </>
   );
 };
 
