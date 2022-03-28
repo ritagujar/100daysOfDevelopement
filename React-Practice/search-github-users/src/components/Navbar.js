0b2b8cd070f3e0738a120d524d6da2cc4d70a7ec
ritagujar <ritagujar14@gmail.com>
2022-03-28 22:07:07 +0530

    Added Navbar

---
+++ React-Practice/search-github-users/src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import { AiFillGithub } from "react-icons/ai";
+import SearchUser from "./SearchUser";
+
+const NavbarSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 4.5rem;
+  background-color: var(--color-black);
+
+  @media screen and (max-width: 650px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+const Heading = styled.div`
+  display: flex;
+  align-items: center;
+  color: var(--color-golden);
+
+  h1 {
+    font-size: 2.1rem;
+    font-family: var(--font-base);
+  }
+`;
+const Logo = styled.a`
+  font-size: 2.5rem;
+  margin-right: 0.5rem;
+`;
+
+const Navbar = () => {
+  return (
+    <NavbarSection>
+      <Heading>
+        <Logo>
+          {" "}
+          <AiFillGithub />
+        </Logo>
+        <h1>Search Github User</h1>
+      </Heading>
+      <SearchUser />
+    </NavbarSection>
+  );
+};
+
+export default Navbar;
---
+++ React-Practice/search-github-users/src/components/SearchUser.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const SearchSection = styled.div``;
+
+const SearchUser = () => {
+  return (
+    <SearchSection>
+      <form action="">
+        <input type="text" />
+      </form>
+    </SearchSection>
+  );
+};
+
+export default SearchUser;
