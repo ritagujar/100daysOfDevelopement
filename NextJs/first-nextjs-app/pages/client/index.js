1b61409a3818a21673788247ed5eed62d3d1a743
ritagujar <ritagujar14@gmail.com>
2022-04-08 23:06:57 +0530

    Navigate With Link Components & Dynamic Routes

--- NextJs/first-nextjs-app/pages/client/index.js
+++ NextJs/first-nextjs-app/pages/client/index.js
@@ -1,9 +1,32 @@
 // http://localhost:3000/client
 
+import Link from "next/link";
+
 function ClientPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>The Client Page</h1>
+      {/* <ul>
+        <li>
+          <Link href="/client/max">Maximilian</Link>
+        </li>
+        <li>
+          <Link href="/client/manu">Manuel</Link>
+        </li>
+      </ul> */}
+
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/client/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- NextJs/first-nextjs-app/pages/index.js
+++ NextJs/first-nextjs-app/pages/index.js
@@ -1,9 +1,19 @@
 // http://localhost:3000
 
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/client">Client</Link>
+        </li>
+      </ul>
     </div>
   );
 }
