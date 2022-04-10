0c0f5e6cffdf95224bb8c5c25d8149a50b2f2f8d
ritagujar <ritagujar14@gmail.com>
2022-04-10 15:48:29 +0530

    Styled Invalid Messages

--- NextJs/events-app/pages/events/[...slug].js
+++ NextJs/events-app/pages/events/[...slug].js
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/results-title/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert/error-alert";
 
 function FilteredEventPage() {
   const router = useRouter();
@@ -26,7 +29,17 @@ function FilteredEventPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values!</p>;
+    return (
+      <>
+        <ErrorAlert>
+          {" "}
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
@@ -35,13 +48,26 @@ function FilteredEventPage() {
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No event found for the chosen filter!</p>;
+    return (
+      <>
+        <ErrorAlert>
+          <p>No event found for the chosen filter!</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
+    <>
+      <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
-    </div>
+    </>
   );
 }
 
