c57e4db0e420a4d48f10dd6b8ce4fa80fa931ed1
ritagujar <ritagujar14@gmail.com>
2022-06-06 22:06:02 +0530

    Using getStaticPaths()

--- NextJs/ppr-data-fetching/pages/index.js
+++ NextJs/ppr-data-fetching/pages/index.js
@@ -1,4 +1,5 @@
 import fs from "fs/promises";
+import Link from "next/link";
 import path from "path";
 
 function HomePage(props) {
@@ -7,7 +8,9 @@ function HomePage(props) {
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
