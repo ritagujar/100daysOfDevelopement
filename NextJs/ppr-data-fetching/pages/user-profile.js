bd972880e7b36e4555c6ad3453f9f0e6bab5d1a2
ritagujar <ritagujar14@gmail.com>
2022-06-07 23:41:16 +0530

    Dynamic pages & getServerSideProps()

--- NextJs/ppr-data-fetching/pages/user-profile.js
+++ NextJs/ppr-data-fetching/pages/user-profile.js
@@ -4,7 +4,12 @@ function UserProfilePage(props) {
 
 export default UserProfilePage;
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  //   console.log(req);
+  //   console.log(res);
+
   return {
     props: {
       username: "Max",
