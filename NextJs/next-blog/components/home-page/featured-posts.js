4ae27f1c889cb059a077232c9902ef7a0b48618a
ritagujar <ritagujar14@gmail.com>
2022-07-22 11:33:52 +0530

    Day79: Started With Home Page

---
+++ NextJs/next-blog/components/home-page/featured-posts.js
@@ -0,0 +1,3 @@
+function FeaturedPosts() {}
+
+export default FeaturedPosts;
--- NextJs/next-blog/pages/index.js
+++ NextJs/next-blog/pages/index.js
@@ -1,5 +1,13 @@
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+
 function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
 }
 
 export default HomePage;
