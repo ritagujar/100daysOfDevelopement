c07aa560b4619436551afaa9d5b019076bc74a0c
ritagujar <ritagujar14@gmail.com>
2022-07-22 11:52:52 +0530

    Added Hero Component

--- NextJs/next-blog/components/home-page/hero.js
+++ NextJs/next-blog/components/home-page/hero.js
@@ -1,3 +1,25 @@
-function Hero() {}
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/rita.jfif"
+          alt="Rita"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I&apos;m Rita</h1>
+      <p>
+        I blog about web development - expecially frontend frameworks like React
+        & Nextjs
+      </p>
+    </section>
+  );
+}
 
 export default Hero;
