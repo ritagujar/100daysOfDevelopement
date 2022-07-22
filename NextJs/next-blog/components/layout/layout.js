2d9e695e5f3373065fca3b187a66b92e73639de0
ritagujar <ritagujar14@gmail.com>
2022-07-22 20:44:21 +0530

    Day79: Added Styling and Logo

--- NextJs/next-blog/components/layout/layout.js
+++ NextJs/next-blog/components/layout/layout.js
@@ -1,10 +1,12 @@
 import MainNavigation from "./main-navigation";
 
 function Layout(props) {
-  <>
-    <MainNavigation />
-    <main>{props.children}</main>
-  </>;
+  return (
+    <>
+      <MainNavigation />
+      <main>{props.children}</main>
+    </>
+  );
 }
 
 export default Layout;
