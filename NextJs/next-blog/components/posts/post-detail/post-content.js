583063027cb5aad908c30a525330119c647347ec
ritagujar <ritagujar14@gmail.com>
2022-07-27 21:15:32 +0530

    Day 82: Rendering Code Snippets from Markdown

--- NextJs/next-blog/components/posts/post-detail/post-content.js
+++ NextJs/next-blog/components/posts/post-detail/post-content.js
@@ -1,4 +1,6 @@
 import classes from "./post-content.module.css";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
@@ -38,6 +40,18 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
